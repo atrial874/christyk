2ddbe2259907ab27e94e015a8b0e97f4876a843e
atrial874 <95119887+atrial874@users.noreply.github.com>
2023-07-25 22:44:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ PICS = (environ.get('PICS' ,'https://te.legra.ph/file/2536d5e8c9f5faa51b253.jpg 
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378 5845960615').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001709366899').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
